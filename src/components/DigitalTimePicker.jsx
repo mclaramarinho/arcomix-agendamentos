29d81515f46b52287aae579368a804e395c3e8a1
mclaramarinho <marinho.claramb@gmail.com>
2023-10-10 13:47:50 -0300

    feat: now scheduled times appear as disabled

--- src/components/DigitalTimePicker.jsx
+++ src/components/DigitalTimePicker.jsx
@@ -1,24 +1,38 @@
 import React from "react";
-import { DigitalClock, LocalizationProvider } from "@mui/x-date-pickers";
+import { DigitalClock, LocalizationProvider, TimeIcon } from "@mui/x-date-pickers";
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { Stack, Typography } from "@mui/material";
+import { MenuItem, Stack, Typography } from "@mui/material";
 import dayjs from 'dayjs';
 
 function DigitalTimePicker(props){
     const minTime = props.minTime;
-    
+
     function shouldDisableTime(value, view){
-        if(view==="hours"){
-            let control = false;
+        let control = false;
+        const currentElement = (document.getElementsByClassName('MuiDigitalClock-item')[value.hour()])
+        
+        if(value.hour()<9 || value.hour() > 19){
+            currentElement.classList.add('hide')
+            control = true;
+        }else{
             props.disabledTimes.map(item => {
                 if(value.hour()===item){
-                    control = true;
+                    currentElement.classList.add('disabled-time')
                     return;
                 }
             })
-            return control;
         }
+        return control;
     }
+    function workingHoursOnly(value, view){
+        let control = false;
+        if(value.hour()<9 || value.hour() > 19){
+            control = true;
+        }
+        return control;
+
+    }
+
     return(
         <LocalizationProvider dateAdapter={AdapterDayjs} >
             <Stack>
@@ -26,12 +40,10 @@ function DigitalTimePicker(props){
                     Horários disponíveis
                 </Typography>
                 <DigitalClock sx={{width:"40%"}} className="digitalClock"
-                    minTime={dayjs().set('hour', minTime).startOf('hour')} maxTime={dayjs().set('hour', 19).startOf('hour')}
                     ampm={false} 
-                    skipDisabled
                     value={props.value}
                     timeStep={60}
-                    shouldDisableTime={shouldDisableTime}
+                    shouldDisableTime={(props.disabledTimes !== undefined && props.disabledTimes.length > 0) ? shouldDisableTime : workingHoursOnly}
                     onChange={async (value, selectionState)=>{
                         props.setValue(dayjs(value).format("HH:mm"));
                         await props.setDateHour(value.$H, value.$m)

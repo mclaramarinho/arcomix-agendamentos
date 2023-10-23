17986d87606c6c4baced8d3aa2d61e2c915faa76
mclaramarinho <marinho.claramb@gmail.com>
2023-10-23 14:14:02 -0300

    style: list container width

--- src/tabs/TabFinalizados.jsx
+++ src/tabs/TabFinalizados.jsx
@@ -84,7 +84,7 @@ function TabFinalizados(){
                         </div>
                     </div>
                 </div>
-                <div className="col-lg-5 col-12 mt-lg-0 mt-5">
+                <div className="col-lg-5 col-12 mt-lg-0 mt-5 ms-lg-auto me-lg-0">
                     <Container tipoContainer="finalizados" lista={resultado} isEmpty={isEmpty}/>
                 </div>
 

43890e8f01e1e0c0bd27f8e4e23dbff5acc14120
mclaramarinho <marinho.claramb@gmail.com>
2023-10-29 20:49:48 -0300

    cy-tests: login page should render correctly

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,61 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+
+describe('login', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
   })
+  
+  it("login page should render correctly", () => {
+    cy.get('[data-cy="login-container"]')
+      .should("exist")
+    cy.get('[data-cy="actor-label"]')
+      .should("exist")
+    cy.get("[data-cy='logo-login']")
+      .should("exist")
+    cy.get("[data-cy='bem-vindo-header']")
+      .should("exist")
+    cy.get("[data-cy='user-input']")
+      .should("exist")
+    cy.get("[data-cy='pswd-input']")
+      .should("exist")
+    cy.get("[data-cy='lembrar-label']")
+      .should("exist")
+    cy.get("[data-cy='esqueci-label']")
+      .should("exist")
+
+    //switch button should exist and be available to click
+    cy.get('[data-cy="switch-actor-btn"]').should("exist").click()
+    //should switch the actor
+    cy.get('[data-cy="actor-label"]')
+      .should(($p) => {
+        expect($p.first()).to.contain("Fornecedor")
+      })
+    //placeholder should change
+    cy.get("[data-cy='user-inputfield'][placeholder='ID Login']")
+      .should("exist")
+
+    //click again
+    cy.get('[data-cy="switch-actor-btn"]').should("exist").click()
+    //switch back to Colaborador
+    cy.get('[data-cy="actor-label"]')
+      .should(($p) => {
+        expect($p.first()).to.contain("Colaborador")
+      })
+    //placeholder should change
+    cy.get("[data-cy='user-inputfield'][placeholder='Matrícula']")
+      .should("exist")
+    
+    //checkbox should be working
+    cy.get("[data-cy='lembrar-check']")
+      .check()
+      .should("be.checked")
+
+
+    cy.get("[data-cy='entrar-btn']")
+      .should("exist")
+      .click()
+    cy.get("[data-cy='auth-error-msg']")
+      .should("exist")
+  })
+
+  // it("login as employee with the correct info")
 })

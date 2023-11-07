2473179389a321a6eebb1644dd6e50e38c77b027
mclaramarinho <marinho.claramb@gmail.com>
2023-11-07 15:31:45 -0300

    feat: added a new tab to the final report

--- src/components/MenuTabs.jsx
+++ src/components/MenuTabs.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import { getTempLoginInfo } from "../utils/tempLoginInfo";
 
 function MenuTabs(props){
     const handleTabClick = () => props.onTabClick;
-
+    const authInfo = getTempLoginInfo();
     return(
         <ul class="nav nav-tabs">
             <li class="nav-item bold text-center">
@@ -20,6 +21,15 @@ function MenuTabs(props){
                     <i class="fa-solid fa-clock-rotate-left"/> FINALIZADOS
                 </a>
             </li>
+            {
+                authInfo.actor === "Colaborador" && (
+                    <li class="nav-item bold text-center">
+                        <a type="button" class="nav-link tab" aria-current="page" href="" onClick={handleTabClick()}>
+                            <i class="fa-solid fa-clock-rotate-left"/> RELATÓRIO GERAL
+                        </a>
+                    </li>   
+                )
+            }
         </ul>
     )
 }

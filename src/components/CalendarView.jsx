d51cc1bc023c3cc921b425f7e4f5cbde3e923720
mclaramarinho <marinho.claramb@gmail.com>
2023-11-16 14:36:34 -0300

    style: full-width calendar view

--- src/components/CalendarView.jsx
+++ src/components/CalendarView.jsx
@@ -1,110 +1,124 @@
-import React, { useEffect } from "react";
-import { Badge, Stack, Typography } from "@mui/material";
-import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
-import { DayCalendarSkeleton } from "@mui/x-date-pickers/DayCalendarSkeleton";
-import { PickersDay } from "@mui/x-date-pickers/PickersDay";
-import { LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import dayjs from "dayjs";
-import { getAgendamentosLS } from "../utils/agendamentosLS";
-import { useState } from "react";
-function CalendarView(props) {
-  let agendamentos = props.lista;
-  agendamentos = agendamentos.filter((item) => item.status === "agendado");
-  const initialValue = dayjs(dayjs().format("YYYY-MM-DD"));
+  import React, { useEffect } from "react";
+  import { Badge, Stack, Typography } from "@mui/material";
+  import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+  import { DayCalendarSkeleton } from "@mui/x-date-pickers/DayCalendarSkeleton";
+  import { PickersDay } from "@mui/x-date-pickers/PickersDay";
+  import { LocalizationProvider } from "@mui/x-date-pickers";
+  import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+  import dayjs from "dayjs";
+  import { useState } from "react";
+  function CalendarView(props) {
+    let agendamentos = props.lista;
+    agendamentos = agendamentos.filter((item) => item.status === "agendado");
+    const initialValue = dayjs(dayjs().format("YYYY-MM-DD"));
 
-  const [isLoading, setIsLoading] = useState(true);
-  const [highlightedDays, setHighlightedDays] = useState([]);
-  const [value, setValue] = useState(initialValue);
+    const [isLoading, setIsLoading] = useState(true);
+    const [highlightedDays, setHighlightedDays] = useState([]);
+    const [value, setValue] = useState(initialValue);
 
-  useEffect(() => {
-    fakeFetch(initialValue).then((response) => {
-      setHighlightedDays(response.daysToHighlight);
-      setIsLoading(false);
-    });
-  }, []);
+    useEffect(() => {
+      fakeFetch(initialValue).then((response) => {
+        setHighlightedDays(response.daysToHighlight);
+        setIsLoading(false);
+      });
+    }, []);
 
-  let agendamentosDoMes = [];
+    let agendamentosDoMes = [];
 
-  function fakeFetch(date) {
-    agendamentosDoMes = agendamentos
-      .map((item) => {
-        if (dayjs(item.data).month() === date.month()) {
-          return item.data;
-        }
-      })
-      .filter((item) => item !== undefined);
+    function fakeFetch(date) {
+      agendamentosDoMes = agendamentos
+        .map((item) => {
+          if (dayjs(item.data).month() === date.month()) {
+            return item.data;
+          }
+        })
+        .filter((item) => item !== undefined);
 
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        const daysToHighlight = agendamentosDoMes.map((item) =>
-          dayjs(item).date()
-        );
-        setHighlightedDays(daysToHighlight);
-        resolve({ daysToHighlight });
-      }, 500);
-    });
-  }
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          const daysToHighlight = agendamentosDoMes.map((item) =>
+            dayjs(item).date()
+          );
+          setHighlightedDays(daysToHighlight);
+          resolve({ daysToHighlight });
+        }, 500);
+      });
+    }
 
-  function handleMonthChange(date) {
-    setIsLoading(true);
-    setHighlightedDays([]);
-    fakeFetch(date).then((response) => {
-      setIsLoading(false);
-    });
-  }
+    function handleMonthChange(date) {
+      setIsLoading(true);
+      setHighlightedDays([]);
+      fakeFetch(date).then((response) => {
+        setIsLoading(false);
+      });
+    }
 
-  function ServerDay(props) {
-    const { day, outsideCurrentMonth, ...other } = props;
+    function ServerDay(props) {
+      const { day, outsideCurrentMonth, ...other } = props;
 
-    const isSelected =
-      !props.outsideCurrentMonth &&
-      highlightedDays.indexOf(props.day.date()) >= 0;
+      const isSelected =
+        !props.outsideCurrentMonth &&
+        highlightedDays.indexOf(props.day.date()) >= 0;
+
+      return (
+        <Badge
+          key={props.day.toString()}
+          overlap="circular"
+          badgeContent={
+            isSelected ? (
+              <i
+                style={{ color: "#68B644", fontSize: 14 }}
+                class="fa-solid fa-circle"
+              ></i>
+            ) : undefined
+          }
+        >
+          <PickersDay
+            {...other}
+            outsideCurrentMonth={outsideCurrentMonth}
+            day={day}
+          />
+        </Badge>
+      );
+    }
+
+    function disabledDays(date, view) {
+      const dayOfWeek = date.day();
+      return dayOfWeek === 0 || dayOfWeek === 6 ? true : false;
+    }
 
     return (
-      <Badge
-        key={props.day.toString()}
-        overlap="circular"
-        badgeContent={
-          isSelected ? (
-            <i
-              style={{ color: "#68B644", fontSize: 14 }}
-              class="fa-solid fa-circle"
-            ></i>
-          ) : undefined
-        }
-      >
-        <PickersDay
-          {...other}
-          outsideCurrentMonth={outsideCurrentMonth}
-          day={day}
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DateCalendar
+          className="h-100"
+          value={value}
+          loading={isLoading}
+          renderLoading={() => <DayCalendarSkeleton />}
+          onMonthChange={handleMonthChange}
+          slots={{ day: ServerDay }}
+          shouldDisableDate={disabledDays}
+          onChange={(newValue) => {
+            props.setDate(newValue);
+            setValue(newValue);
+          }}
+          sx={{
+            margin: "0 !important",
+            width: "100% !important",
+            position: "relative !important",
+            "& .MuiPickersDay-root": {
+              position: "relative !important",
+              maxWidth: "12vw !important",
+            },
+            "& .MuiDayCalendar-weekContainer, .MuiDayCalendar-header": {
+              width: "100% !important",
+              boxSizing: "border-box !important",
+              justifyContent: "space-evenly !important",
+            },
+            
+          }}      
         />
-      </Badge>
+      </LocalizationProvider>
     );
   }
 
-  function disabledDays(date, view) {
-    const dayOfWeek = date.day();
-    return dayOfWeek === 0 || dayOfWeek === 6 ? true : false;
-  }
-
-  return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DateCalendar
-        className="h-100"
-        value={value}
-        loading={isLoading}
-        renderLoading={() => <DayCalendarSkeleton />}
-        onMonthChange={handleMonthChange}
-        slots={{ day: ServerDay }}
-        shouldDisableDate={disabledDays}
-        onChange={(newValue) => {
-          props.setDate(newValue);
-          setValue(newValue);
-        }}
-      />
-    </LocalizationProvider>
-  );
-}
-
-export default CalendarView;
+  export default CalendarView;

e66079dc44f37290df8357c3d6f52758f7f19678
Clara Marinho <119897667+mclaramarinho@users.noreply.github.com>
2023-11-24 08:56:23 -0300

    Update colaboradores.js

--- src/users/colaboradores.js
+++ src/users/colaboradores.js
@@ -16,16 +16,7 @@ const colaboradores = [
             email: "email@gmail.com"
         },
         profile_pic: "https://images.rawpixel.com/image_png_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIzLTA4L3Jhd3BpeGVsX29mZmljZV8xMl9waG90b19vZl9nb2xkZW5fcmV0cmlldmVyX3B1cHB5X2p1bXBpbmdfaXNvbF83MTM2NGE2OS1kZTM0LTQzMWEtYWRkZS04ZTdmZWQ0ZGFiOTIucG5n.png"
-    },
-    {
-       matricula:"1234567",
-       senha:"alexandre",
-       informacoes:{   
-           nome: "Alexandre Santana",
-           email: "email@gmail.com"
-       },
-       profile_pic: "https://w7.pngwing.com/pngs/761/610/png-transparent-account-avatar-human-people-person-profile-user-random-icon.png"
-   }
+    }
 ]
 
 export default colaboradores;
